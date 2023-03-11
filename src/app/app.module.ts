86d653ac96e9acafe9cd8297b0816ac432c60009
Imran S <55154759+ImranS19@users.noreply.github.com>
2023-03-11 14:13:55 -0500

    current weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CurentWeatherComponent } from './curent-weather/curent-weather.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurentWeatherComponent,
+    CurrentWeatherComponent
   ],
   imports: [
     BrowserModule
